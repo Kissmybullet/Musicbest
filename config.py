db4d601ef99d947ab593dc5ac80ba6640d57e946
Kissmybullet <128668418+Kissmybullet@users.noreply.github.com>
2024-03-18 14:18:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Dinobonecrash1/Musicbest",
+    "https://github.com/Kissmybullet/Musicbest",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
